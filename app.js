d027171bc585664014a1b2eecba404e9905f4d4e
kprocks <laniyakrunal@gmail.com>
2017-07-15 21:21:25 +0530

    add env-variable for PORT & IP

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ app.set('view engine','ejs');
 keyListnerMain(app);
 app.use(express.static('./public'));
 
-app.listen(5000);
-console.log('listen to the port:5000');
+app.listen(process.env.PORT,process.env.IP,function () {
+	console.log('server just started!');
+});
+
